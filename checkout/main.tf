4318612429feca2c9ac169316d233300fcff6584
Ayushi Saxena <Ayushi.Saxena@insight.com>
2025-08-19 13:25:19 +0530

    added paths in main

--- checkout/main.tf
+++ checkout/main.tf
@@ -3,26 +3,26 @@ provider "aws" {
 }
 
 module "vpc_creation" {
-  source = "./VPC_Module"
+  source = "../VPC_Module"
   vpc_block = var.range
   vpc_git = var.vpc_name
   region = var.aws_region
   
 }
 module "subnet1_creation" {
-  source = "./subnet_module"
+  source = "../subnet_module"
   vpc_id = module.vpc_creation.vpc_id
   cidr_block = var.cidr_block_1
   aws_region = var.aws_region
 }
 module "subnet2_creation" {
-  source = "./subnet_module"
+  source = "../subnet_module"
   vpc_id = module.vpc_creation.vpc_id
   cidr_block = var.cidr_block_2
   aws_region = var.aws_region
 }
 module "ec2_instance" {
-    source = "./EC2"
+    source = "../EC2"
    instance_type_value = var.instance_type_value
    ami_id = var.ami_id
    instance_name = var.instance_name
